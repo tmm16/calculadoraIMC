8135aa7f7d6650c11f7d6258fafd019e1c96c88e
Thiago Mota <thiagao91@gmail.com>
2019-05-05 10:55:14 -0300

    Cálculo de Peso Ideal
    Agora o sistema também mostra qual a faixa de peso ideal para o usuário.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,43 +20,48 @@ $(function() {
         var altura = $('#altura').val();
         var peso = $('#peso').val();
         var imc = peso / (altura * altura);
+        /*Calcula qual o peso ideal de acordo com a altura*/
+        var minpesoideal = (altura * altura) * 18.5;
+        var maxpesoideal = (altura * altura) * 24.99;
 
         /*Ajusta o sistema para aceitar virgula ou ponto*/
         altura = altura.replace(',','.');
         peso = peso.replace(',','.');
         /*Apresenta apenas duas casas decimais*/
         imc = imc.toFixed(2);
+        minpesoideal = minpesoideal.toFixed(2);
+        maxpesoideal = maxpesoideal.toFixed(2);
 
         /*Preenche resultado*/
         $('#resultado').html("Seu IMC é: "+imc+".");
 
         /*Preenche orientação e atualiza a imagem*/
         if(imc < 18.5) {
-            $('#orientacao').html('Você está abaixo do peso!');
+            $('#orientacao').html('Você está abaixo do peso! <br/> Seu peso ideal está entre '+minpesoideal+" kg e "+maxpesoideal+" kg.<br/>");
             $("#imagem").attr("src", "assets/images/abaixo.png");
         }
         else if(imc >= 18.5 && imc < 25) {
-            $('#orientacao').html('Parabéns — você está em seu peso normal!');
+            $('#orientacao').html('Parabéns — você está em seu peso normal! <br/> Seu peso ideal está entre '+minpesoideal+" kg e "+maxpesoideal+" kg.<br/>");
             $("#imagem").attr("src", "assets/images/normal.png");
         }
         else if(imc >= 25 && imc < 30) {
-            $('#orientacao').html('Você está acima de seu peso (sobrepeso).');
+            $('#orientacao').html('Você está acima de seu peso (sobrepeso). <br/> Seu peso ideal está entre '+minpesoideal+" kg e "+maxpesoideal+" kg.<br/>");
             $("#imagem").attr("src", "assets/images/sobrepeso.png");
         }
         else if(imc >= 30 && imc < 35) {
-            $('#orientacao').html('Obesidade grau I.');
+            $('#orientacao').html('Obesidade grau I. <br/> Seu peso ideal está entre '+minpesoideal+" kg e "+maxpesoideal+" kg.<br/>");
             $("#imagem").attr("src", "assets/images/obeso1.png");
         }
         else if(imc >= 35 && imc < 40) {
-            $('#orientacao').html('Obesidade grau II.');
+            $('#orientacao').html('Obesidade grau II. <br/> Seu peso ideal está entre '+minpesoideal+" kg e "+maxpesoideal+" kg.<br/>");
             $("#imagem").attr("src", "assets/images/obeso2.png");
         }
         else if(imc >= 40 && imc < 50) {
-            $('#orientacao').html('Obesidade grau III.');
+            $('#orientacao').html('Obesidade grau III. <br/> Seu peso ideal está entre '+minpesoideal+" kg e "+maxpesoideal+" kg.<br/>");
             $("#imagem").attr("src", "assets/images/obeso3.png");
         }
         else if(imc > 50) {
-            $('#orientacao').html('Obesidade grau IV.');
+            $('#orientacao').html('Obesidade grau IV. <br/> Seu peso ideal está entre '+minpesoideal+" kg e "+maxpesoideal+" kg.<br/>");
             $("#imagem").attr("src", "assets/images/obeso4.png");
         }
         else {
@@ -65,5 +70,4 @@ $(function() {
         }
 
     });
-
 });
